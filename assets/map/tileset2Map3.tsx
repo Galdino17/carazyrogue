084daffe849ecf6c0d3a9c5f570f5ec1dc78d185
Galdino <galdino17@gmail.com>
2026-04-10 11:22:36 -0300

    Tamanho das letras HUD e Market

---
+++ assets/map/tileset2Map3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="map1" tilewidth="64" tileheight="64" tilecount="768" columns="32">
+ <image source="map3I2.png" width="2048" height="1536"/>
+</tileset>
---
+++ assets/map/tilesetMap3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Untitled (1)" tilewidth="64" tileheight="64" tilecount="768" columns="32">
+ <image source="map3I1.png" width="2048" height="1536"/>
+</tileset>
